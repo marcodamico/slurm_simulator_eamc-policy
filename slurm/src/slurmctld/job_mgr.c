83bf3750236f7552dc32635e821d0228a8e9d73e
ana jokanovic <ana.jokanovic@bsc.es>
2019-07-22 12:45:12 +0200

    Tuning priorities of alternative jobs; at the moment commented out

--- slurm/src/slurmctld/job_mgr.c
+++ slurm/src/slurmctld/job_mgr.c
@@ -4787,9 +4787,27 @@ extern int job_allocate(job_desc_msg_t * job_specs, int immediate,
 	 * begin time in job_independent and that lets us know if the
 	 * job is eligible.
 	 */
-	if (job_ptr->priority == NO_VAL)
+	if (job_ptr->priority == NO_VAL){
 		set_job_prio(job_ptr);
-
+                /* This part was added to tune priorities of alternative jobs in plussingleton dependecy case TODO
+                  if(job_ptr->details->depend_list){
+                   if(!strcmp(job_ptr->name,prev_name)){
+                     job_dep_count++;
+                     if(job_dep_count == 2) job_ptr->priority = job_ptr->priority - 14;
+                     else if (job_dep_count == 3) {
+                         job_ptr->priority = job_ptr->priority - 15; // 50 and 100 should be made parameters to control how fast do we want alternative modules to be considered.
+                         //job_dep_count=0;
+                     }
+                     strcpy(prev_name,job_ptr->name);
+                     info("JOB_MGR: Setting priority to %u for job %u, order number %u ", job_ptr->priority, job_ptr->job_id, job_dep_count);
+                   }
+                   else{
+                       job_dep_count=1;
+                       strcpy(prev_name,job_ptr->name);
+                   }
+                }
+*/
+        }
 	if (independent &&
 	    (license_job_test(job_ptr, time(NULL), true) != SLURM_SUCCESS))
 		independent = false;
